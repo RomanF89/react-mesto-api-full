75297508d6c437d3cc920697119e3e396d3c2f47
Roman Faritov <romko_1992@mail.ru>
2022-06-27 05:40:37 +0500

    front+back

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 const { BadAuthError } = require('../errors/badAuthError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new BadAuthError('Need authorization'));
   }
